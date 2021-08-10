55c550749676f60cc83ab6f1c556f91f15cf2498
Irene Vera <irene.vera@webbeds.com>
2021-08-10 16:10:39 +0200

    Submit crea nuevo state.

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// importamos los components
+import Todo from './Todo'
+
+function TodoList() {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
